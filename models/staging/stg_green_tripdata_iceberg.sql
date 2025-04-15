8c2ab2428429f77d792c1a1344c476db10daa77e
Ayodeji Lana <lanaayodeji10@gmail.com>
2025-04-15 03:54:49 +0000

    Successfully built the first model.

--- models/staging/stg_green_tripdata_iceberg.sql
+++ models/staging/stg_green_tripdata_iceberg.sql
@@ -26,18 +26,17 @@ select
     -- trip info
     store_and_fwd_flag,
     {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
+    cast(trip_distance as Double) as trip_distance,
     {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
 
     -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
+    cast(fare_amount as Double) as fare_amount,
+    cast(extra as Double) as extra,
+    cast(mta_tax as Double) as mta_tax,
+    cast(tip_amount as Double) as tip_amount,
+    cast(tolls_amount as Double) as tolls_amount,
+    cast(improvement_surcharge as Double) as improvement_surcharge,
+    cast(total_amount as Double) as total_amount,
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
     {{ get_payment_type_description("payment_type") }} as payment_type_description
 from tripdata
